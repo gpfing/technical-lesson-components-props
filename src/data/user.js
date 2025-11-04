f9091954ca0313793dad2a39a55bdf6762010d67
Garrett Pfingston <gpfingston@gmail.com>
2025-11-03 21:03:18 -0600

    practice lab

--- src/data/user.js
+++ src/data/user.js
@@ -1,11 +1,11 @@
 const user = {
-  name: "Liza",
-  city: "New York",
+  name: "Garrett",
+  city: "Dallas",
   bio: "I made this!",
   color: "firebrick",
   links: {
-    github: "https://github.com/liza",
-    linkedin: "https://www.linkedin.com/in/liza/",
+    github: "https://github.com/gpfing",
+    linkedin: "https://www.linkedin.com/in/garrett-pfingston",
   },
 };
 
